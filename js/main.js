f6e76ee99ebe4c094a429168a98bd4b8e4284584
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-03-23 01:50:20 -0300

    ui/ux

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const home = document.getElementById('sectionHomeContainer')
-const game = document.getElementById('game')
-const player1 = document.getElementById('inputplayer1')
-const player2 = document.getElementById('inputplayer2')
-const nameplayer1 = document.getElementById('nameplayer1')
-const nameplayer2 = document.getElementById('nameplayer2')
-document.getElementById('startButton').addEventListener('click', start)
+const home = document.querySelector('.sectionHomeContainer')
+const game = document.querySelector('.sectionGameContainer')
+const player1 = document.querySelector('#inputplayer1')
+const player2 = document.querySelector('#inputplayer2')
+const nameplayer1 = document.querySelector('#nameplayer1')
+const nameplayer2 = document.querySelector('#nameplayer2')
+const choicePlayer1 = document.querySelector('#choicePlayer1')
+document.querySelector('#startButton').addEventListener('click', start)
 
 function start() {
   if (player1.value == '' || player2.value == '') {
@@ -17,10 +18,3 @@ function start() {
     nameplayer2.innerText = player2.value
   }
 }
-
-function hidden1() {
-  home.style.display = 'none'
-  game.style.display = 'flex'
-}
-
-//faca amor nao faca guerra
